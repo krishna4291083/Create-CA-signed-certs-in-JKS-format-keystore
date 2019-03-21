d3b7ca052276ed2d7cdaa0c443a6591c5b86b960
Krishnaraj V <krishna4291083@gmail.com>
2019-03-21 13:55:10 +0530

    validity is missed in crt generation

--- ca_keystore_create.py
+++ ca_keystore_create.py
@@ -35,9 +35,9 @@ def keystore_create_rootca():
 def keystore_create_jks(host):
   host_dn_format=host_dn.format(host)
   print("====Creating keystore for the host : %s====" % (host))
-  cmd_cert="keytool -genkeypair -v -alias {0} -dname {2} -keystore {0}.jks -keypass {1} -storepass {1} -keyalg RSA -keysize 2048 -validity 385".format(host,password,host_dn_format)
+  cmd_cert="keytool -genkeypair -v -alias {0} -dname {2} -keystore {0}.jks -keypass {1} -storepass {1} -keyalg RSA -keysize 2048 -validity 365".format(host,password,host_dn_format)
   cmd_csr="keytool -certreq -v -alias {0} -keypass {1} -storepass {1} -keystore {0}.jks -file {0}.csr".format(host,password)
-  cmd_crt="keytool -gencert -v -alias rootca -keypass {1} -storepass {1} -keystore rootca.jks -infile {0}.csr -outfile {0}.crt -ext KeyUsage:critical=\"digitalSignature,keyEncipherment\" -ext EKU=\"serverAuth,clientAuth\" -ext SAN=\"DNS:{0}\" -rfc".format(host,password)
+  cmd_crt="keytool -gencert -v -alias rootca -keypass {1} -storepass {1} -keystore rootca.jks -infile {0}.csr -outfile {0}.crt -ext KeyUsage:critical=\"digitalSignature,keyEncipherment\" -ext EKU=\"serverAuth,clientAuth\" -ext SAN=\"DNS:{0}\" -validity 365 -rfc".format(host,password)
   cmd_keystore_sign="keytool -import -v -alias {0} -file {0}.crt -keystore {0}.jks -storetype JKS -storepass {1}".format(host,password)
   cmd_trust="keytool -import -v -alias rootca -file rootca.crt -keystore {0}.jks -storetype JKS -storepass {1} -noprompt".format(host,password)
 
